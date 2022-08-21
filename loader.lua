98a89b0dfc2c469b1da44a8664b5954e363e9e76
SunnieHub <111681065+SunnieHub@users.noreply.github.com>
2022-08-21 19:00:54 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+repeat wait() until game:IsLoaded()
+local PlaceId = game.PlaceId
+if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/SunnieHub/SunnieHub/main/Bloxfruit.lua")()
+repeat wait() until game:IsLoaded()
+elseif PlaceId == 914010731 then
+    loadstring(game:HttpGet"")()
+end
