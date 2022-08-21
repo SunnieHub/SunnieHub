302c99338518901bd3a8841c37fc517814af63cb
SunnieHub <111681065+SunnieHub@users.noreply.github.com>
2022-08-21 20:01:02 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,5 +4,5 @@ if PlaceId == 2753915549 or PlaceId == 4442272183 or PlaceId == 7449423635 then
     loadstring(game:HttpGet"https://raw.githubusercontent.com/SunnieHub/SunnieHub/main/Bloxfruit.lua")()
 repeat wait() until game:IsLoaded()
 elseif PlaceId == 914010731 then
-    loadstring(game:HttpGet"")()
+    loadstring(game:HttpGet"https://raw.githubusercontent.com/SunnieHub/Sunniehub/main/roghoulLOADER.lua")()
 end
